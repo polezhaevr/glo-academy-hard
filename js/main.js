dbf4940f5a88c31f64f52922f21cecf0290ee592
polezhaevr <mrboogie8995@gmail.com>
2021-04-06 23:24:09 +0500

    add task b,c

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-let weekRu = ['Понедельник' , 'Вторник' , 'Среда' , 'Четверг' , 'Пятница' , 'Суббота' , 'Воскресенье'] , 
-    weekEng = [' Tuesday' , ' Wednesday' , ' Thursday' , ' Friday' , ' Saturday' , ' Sunday'], 
-    lang = prompt ('Введите язык ru или en:');
-    
+let weekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    weekEng = [' Tuesday', ' Wednesday', ' Thursday', ' Friday', ' Saturday', ' Sunday'],
+    lang = prompt('Введите язык ru или en:');
+
+
+/*Вариант с */
+const langArr = {
+    'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'en':[' Tuesday', ' Wednesday', ' Thursday', ' Friday', ' Saturday', ' Sunday']
+}; 
+
+alert(langArr[lang]);
+
+/* конец варианта - с */
+
+  /*  Вариант решения - b
+switch (lang) {
+    case 'ru':
+        alert(weekRu);
+        break;
+    case 'en':
+        alert(weekEng);
+        break;
+        default:
+            alert('Ошибка');
+}
+*/
+
+/* Вариант решения - a 
 if (lang === 'ru') {
     alert(weekRu);
 }else if (lang === 'en') {
@@ -9,8 +34,11 @@ if (lang === 'ru') {
 }else {
     alert('Ошибка');
 }; 
+*/
+
+
 
 let namePerson = prompt('Введите имя:'),
-    result = namePerson === 'Артем' ? alert('Директор') : 
-        (namePerson === 'Максим') ? alert('Преподаватель') : 
-        alert ('Студент') ;
+    result = namePerson === 'Артем' ? alert('Директор') :
+    (namePerson === 'Максим') ? alert('Преподаватель') :
+    alert('Студент');
